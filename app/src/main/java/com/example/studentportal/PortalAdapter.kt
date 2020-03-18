83c63c6ba5aa6e6959bdd0d2157d9c58800c61f1
AngeloC98 <acabrasnl@gmail.com>
2020-03-18 15:16:10 +0100

    Changed layout and added chrome tab

--- app/src/main/java/com/example/studentportal/PortalAdapter.kt
+++ app/src/main/java/com/example/studentportal/PortalAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.studentportal
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_portal.view.*
 
-class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
+class PortalAdapter(private val portals: List<Portal>) :
+    RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PortalAdapter.ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false)
@@ -25,6 +28,12 @@ class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<Po
         fun bind(portal: Portal) {
             itemView.tvTitleItem.text = portal.title
             itemView.tvUrlItem.text = portal.url
+            itemView.setOnClickListener {
+                val builder = CustomTabsIntent.Builder()
+                builder.setShowTitle(true)
+                val customTabsIntent = builder.build()
+                customTabsIntent.launchUrl(itemView.context, Uri.parse(portal.url))
+            }
         }
     }
 }
